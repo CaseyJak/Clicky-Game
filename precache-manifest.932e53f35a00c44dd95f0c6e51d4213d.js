6025bebc8000d44464cb6fd514ba300b56366cce
Casey Jak <casey.j.lee@gmail.com>
2018-12-22 13:26:53 -0600

    Updates

---
+++ precache-manifest.932e53f35a00c44dd95f0c6e51d4213d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a353ce0d5f6d19b7e98",
+    "url": "/CaseyJak/Clicky-Game/static/js/runtime~main.3a353ce0.js"
+  },
+  {
+    "revision": "1465ce591f930fcb509f",
+    "url": "/CaseyJak/Clicky-Game/static/js/main.1465ce59.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/CaseyJak/Clicky-Game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "1465ce591f930fcb509f",
+    "url": "/CaseyJak/Clicky-Game/static/css/main.42fd8515.chunk.css"
+  },
+  {
+    "revision": "d420095948dc17f6068504f68353ca5e",
+    "url": "/CaseyJak/Clicky-Game/index.html"
+  }
+];
